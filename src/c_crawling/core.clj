fecd15b4007bbf12d608064d207a8607386d6716
u4bi <admin@naver.id>
2018-05-15 12:46:33 +0900

    Initialize core.clj

---
+++ src/c_crawling/core.clj
@@ -0,0 +1,18 @@
+(ns c-crawling.core
+  (:use ring.util.response)
+  (:require [compojure.core :refer :all]
+            [compojure.handler :as h]
+            [compojure.route :as route]
+            [ring.middleware.json :as m-json]
+            [ring.middleware.cors :refer [wrap-cors]]))
+
+(defroutes handler
+           (GET "/" [id] "crawling")
+           (route/not-found (response {:message "not found"})))
+
+(def app
+  (-> (h/api handler)
+      (m-json/wrap-json-params)
+      (m-json/wrap-json-response)
+      (wrap-cors :access-control-allow-origin [#"http://127.0.0.1:5500"] ; #".*"
+                 :access-control-allow-methods [:get :put :post :delete])))
