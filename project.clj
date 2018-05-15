6a253744e7a8b709805081d952b2b9fc96699810
u4bi <admin@naver.id>
2018-05-15 12:43:03 +0900

    Added clj-http and enlive to project.clj

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
                  [ring/ring-jetty-adapter "1.6.3"]
                  [compojure "1.6.1"]
                  [ring/ring-json "0.4.0"]
-                 [ring-cors "0.1.12"]]
+                 [ring-cors "0.1.12"]
+                 [clj-http "3.9.0"]
+                 [enlive "1.1.6"]]
   :plugins [[lein-ring "0.12.4"]]
   :ring {:handler c-crawling.core/app})
