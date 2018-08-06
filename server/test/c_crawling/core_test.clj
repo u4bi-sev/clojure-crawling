86256177b2a556bf747ceda1846a6a098c568a6c
myungjaeyu <rootcode10@gmail.com>
2018-08-07 01:48:04 +0900

    Added tests.

--- server/test/c_crawling/core_test.clj
+++ server/test/c_crawling/core_test.clj
@@ -1,7 +1,14 @@
 (ns c-crawling.core-test
   (:require [clojure.test :refer :all]
-            [c-crawling.core :refer :all]))
+            [clj-http.client :as client]
+            [c-crawling.core :refer [body-parser get-title]]))
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 1 1))))
+
+(deftest title-test
+  (testing "dcleaner title")
+  (is (= (get-title
+            (body-parser "https://dcleaner.github.io/"))
+          "DCleaner - 디시 클리너")))
